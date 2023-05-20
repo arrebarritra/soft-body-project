37f9b82368c1b7055ba1867080384e0ab0596e26
aritra <aritra@kth.se>
2023-05-20 21:14:13 +0200

    Add tet mesh surface rendering, compute shader test

--- Assets/Scripts/SoftBody.cs
+++ Assets/Scripts/SoftBody.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-struct Particle
+public struct Particle
 {
-    Vector3 x, xm1, v;
+    public Vector3 x, xm1, v;
+    public float invm;
 }
 
 struct LengthConstraint
 {
-    uint p1, p2;
-    float l0;
+    public int p1, p2;
+    public float l0;
 }
 
 struct VolumeConstraint
 {
-    uint p1, p2, p3, p4;
-    float V0t6;
+    public int p1, p2, p3, p4;
+    public float V0t6;
 }
 
 
@@ -34,7 +35,9 @@ public class SoftBody : MonoBehaviour
     int nParticles;
     int nEdges;
     int nTets;
+    int nTriangles;
 
+    Vector3[] vertices;
     Particle[] particles;
     LengthConstraint[] lc;
     VolumeConstraint[] vc;
@@ -44,20 +47,31 @@ public class SoftBody : MonoBehaviour
     ComputeBuffer lcBuffer;
     ComputeBuffer vcBuffer;
 
+    // Surface mesh for display
+    Mesh mesh;
+    MeshFilter mf;
+
 
     void Start()
     {
         // Initialise soft body data from tetrahedral mesh
         InitializeDataFromTetMesh();
 
+        // Set int parameters
+        shader.SetInt("nParticles", nParticles);
+        shader.SetInt("nEdges", nEdges);
+        shader.SetInt("nTets", nTets);
+        shader.SetInt("nTriangles", nTriangles);
+
         // Create buffers
-        particleBuffer = new ComputeBuffer(nParticles, 3 * 3 * sizeof(float));
+        particleBuffer = new ComputeBuffer(nParticles, (3 * 3 + 1) * sizeof(float));
         lcBuffer = new ComputeBuffer(nEdges, 2 * sizeof(uint) + sizeof(float));
         vcBuffer = new ComputeBuffer(nTets, 4 * sizeof(uint) + sizeof(float));
 
         // Constraint buffers are constant and can already be set
         lcBuffer.SetData(lc);
         vcBuffer.SetData(vc);
+        particleBuffer.SetData(particles);
 
         // Bind buffers
         kiPreSolve = shader.FindKernel("preSolve");
@@ -82,34 +96,126 @@ public class SoftBody : MonoBehaviour
 
         for (int i = 0; i < nSubSteps; i++)
         {
-            shader.Dispatch(kiPreSolve, nParticles / 128, 1, 1);
-            shader.Dispatch(kiSolveEdges, nEdges / 128, 1, 1);
-            shader.Dispatch(kiSolveVolumes, nTets / 128, 1, 1);
-            shader.Dispatch(kiPostSolve, nParticles / 128, 1, 1);
+            // Dispatch with one thread per particle/constraint
+            shader.Dispatch(kiPreSolve, (nParticles + 127) / 128, 1, 1);
+            shader.Dispatch(kiSolveEdges, (nEdges + 127) / 128, 1, 1);
+            shader.Dispatch(kiSolveVolumes, (nTets + 127) / 128, 1, 1);
+            shader.Dispatch(kiPostSolve, (nParticles + 127) / 128, 1, 1);
         }
 
         particleBuffer.GetData(particles);
+        UpdateMeshVertices();
     }
 
     void InitializeDataFromTetMesh()
     {
-        InitializeParticles();
-        InitializeLengthConstraints();
-        InitializeVolumeConstraints();
+        TetMesh tm = new TetMesh();
+        tm.LoadTestModel();
+
+        InitializeParticles(tm);
+        InitializeLengthConstraints(tm);
+        InitializeVolumeConstraints(tm);
+        InitializeSurfaceMesh(tm);
     }
 
-    void InitializeParticles()
+    void InitializeParticles(in TetMesh tm)
     {
-
+        nParticles = tm.nVertices;
+        particles = new Particle[tm.nVertices];
+        for (int i = 0; i < nParticles; i++)
+        {
+            // Set particle positions according to tet mesh
+            particles[i] = new Particle
+            {
+                x = new Vector3(tm.vertices[3 * i], tm.vertices[3 * i + 1], tm.vertices[3 * i + 2]),
+                xm1 = new Vector3(tm.vertices[3 * i], tm.vertices[3 * i + 1], tm.vertices[3 * i + 2]),
+                v = Vector3.zero
+            };
+        }
     }
 
-    void InitializeLengthConstraints()
+    void InitializeLengthConstraints(in TetMesh tm)
     {
+        nEdges = tm.nEdges;
+        lc = new LengthConstraint[nEdges];
+        for (int i = 0; i < nEdges; i++)
+        {
+            // Assign edge points
+            lc[i] = new LengthConstraint
+            {
+                p1 = tm.edgeIndices[2 * i],
+                p2 = tm.edgeIndices[2 * i + 1],
+            };
 
+            // Calculate edge length
+            Vector3 e1 = new Vector3(tm.vertices[3 * lc[i].p1], tm.vertices[3 * lc[i].p1 + 1], tm.vertices[3 * lc[i].p1 + 2]);
+            Vector3 e2 = new Vector3(tm.vertices[3 * lc[i].p2], tm.vertices[3 * lc[i].p2 + 1], tm.vertices[3 * lc[i].p2 + 2]);
+            lc[i].l0 = (e2 - e1).magnitude;
+        }
     }
 
-    void InitializeVolumeConstraints()
+    void InitializeVolumeConstraints(in TetMesh tm)
     {
+        nTets = tm.nTets;
+        vc = new VolumeConstraint[nTets];
+        for (int i = 0; i < nTets; i++)
+        {
+            // Assign tet points
+            vc[i] = new VolumeConstraint
+            {
+                p1 = tm.tetIndices[4 * i],
+                p2 = tm.tetIndices[4 * i + 1],
+                p3 = tm.tetIndices[4 * i + 2],
+                p4 = tm.tetIndices[4 * i + 3],
+            };
 
+            // Calculate volume
+            Vector3 e1 = new Vector3(tm.vertices[3 * vc[i].p1], tm.vertices[3 * vc[i].p1 + 1], tm.vertices[3 * vc[i].p1 + 2]);
+            Vector3 e2 = new Vector3(tm.vertices[3 * vc[i].p2], tm.vertices[3 * vc[i].p2 + 1], tm.vertices[3 * vc[i].p2 + 2]);
+            Vector3 e3 = new Vector3(tm.vertices[3 * vc[i].p3], tm.vertices[3 * vc[i].p3 + 1], tm.vertices[3 * vc[i].p3 + 2]);
+            Vector3 e4 = new Vector3(tm.vertices[3 * vc[i].p4], tm.vertices[3 * vc[i].p4 + 1], tm.vertices[3 * vc[i].p4 + 2]);
+
+            Vector3[] temp = new Vector3[4];
+            temp[0] = e2 - e1;
+            temp[1] = e3 - e1;
+            temp[2] = e4 - e1;
+            temp[3] = Vector3.Cross(temp[0], temp[1]);
+            vc[i].V0t6 = Vector3.Dot(temp[3], temp[2]);
+
+            // Add inv masses to particles
+            float pInvMass = vc[i].V0t6 > 0.0f ? 1.0f / (vc[i].V0t6 / (6.0f * 4.0f)) : 0.0f;
+            particles[vc[i].p1].invm = pInvMass;
+            particles[vc[i].p2].invm = pInvMass;
+            particles[vc[i].p3].invm = pInvMass;
+            particles[vc[i].p4].invm = pInvMass;
+        }
+    }
+
+    void InitializeSurfaceMesh(in TetMesh tm)
+    {
+        mesh = new Mesh();
+        mesh.name = "Soft Body Mesh";
+        mf = GetComponent<MeshFilter>();
+        vertices = new Vector3[nParticles];
+
+        mesh.vertices = new Vector3[nParticles];
+        for (int i = 0; i < nParticles; i++)
+        {
+            mesh.vertices[i] = particles[i].x;
+        }
+
+        nTriangles = tm.nTriangles;
+        mesh.triangles = tm.surfaceTriangleIndices;
+        mf.mesh = mesh;
+    }
+
+    void UpdateMeshVertices()
+    {
+        for (int i = 0; i < nParticles; i++)
+        {
+            vertices[i] = particles[i].x;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
     }
 }
---
+++ Assets/Scripts/TetMesh.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TetMesh
+{
+    public int nVertices { get; private set; }
+    public int nTets { get; private set; }
+    public int nEdges { get; private set; }
+    public int nTriangles { get; private set; }
+    public float[] vertices { get; private set; }
+    public int[] tetIndices { get; private set; }
+    public int[] edgeIndices { get; private set; }
+    public int[] surfaceTriangleIndices { get; private set; }
+
+    public TetMesh()
+    {
+    }
+
+    public void LoadFromFile(string file)
+    {
+        using(TextReader reader = File.OpenText(file))
+        {
+            nVertices = int.Parse(reader.ReadLine());
+            nTets = int.Parse(reader.ReadLine());
+            nEdges = int.Parse(reader.ReadLine());
+            nTriangles = int.Parse(reader.ReadLine());
+
+            vertices = new float[nVertices * 3];
+            for(int  i = 0; i < nVertices * 3; i++)
+            {
+                vertices[i] = float.Parse(reader.ReadLine());
+            }
+
+            tetIndices = new int[nTets * 4];
+            for (int i = 0; i < nTets * 4; i++)
+            {
+                tetIndices[i] = int.Parse(reader.ReadLine());
+            }
+
+            edgeIndices = new int[nEdges * 2];
+            for (int i = 0; i < nEdges * 2; i++)
+            {
+                edgeIndices[i] = int.Parse(reader.ReadLine());
+            }
+
+            surfaceTriangleIndices = new int[nTriangles * 3];
+            for (int i = 0; i < nTriangles * 3; i++)
+            {
+                surfaceTriangleIndices[i] = int.Parse(reader.ReadLine());
+            }
+        }
+    }
+
+    public void LoadTestModel()
+    {
+        LoadFromFile("Assets/TetMeshes/Bunny.txt");
+    }
+}
