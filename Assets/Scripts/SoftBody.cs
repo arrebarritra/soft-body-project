ede90b469d5fd170ed57162e1105ff41a35f82ce
aritra <aritra@kth.se>
2023-05-23 09:22:50 +0200

    Cluster constraints

--- Assets/Scripts/SoftBody.cs
+++ Assets/Scripts/SoftBody.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using QuikGraph;
+using QuikGraph.Algorithms.VertexColoring;
 
 public struct Particle
 {
@@ -34,6 +37,7 @@ public class SoftBody : MonoBehaviour
     static int kiSolveVolumes;
     static int kiPostSolve;
 
+    // Particles and  constraints
     int nParticles;
     int nEdges;
     int nTets;
@@ -44,10 +48,23 @@ public class SoftBody : MonoBehaviour
     LengthConstraint[] lc;
     VolumeConstraint[] vc;
 
+    // Constraint cluster variables
+    int nEdgeClusters;
+    int[] edgesInCluster;
+    int[] edgeClusters;
+
+    int nTetClusters;
+    int[] tetsInCluster;
+    int[] tetClusters;
+
     // Buffers to pass to shader
     ComputeBuffer particleBuffer;
     ComputeBuffer lcBuffer;
     ComputeBuffer vcBuffer;
+    ComputeBuffer eicBuffer;
+    ComputeBuffer ecsBuffer;
+    ComputeBuffer ticBuffer;
+    ComputeBuffer tcsBuffer;
 
     // Surface mesh for display
     Mesh mesh;
@@ -66,16 +83,26 @@ public class SoftBody : MonoBehaviour
         shader.SetInt("nEdges", nEdges);
         shader.SetInt("nTets", nTets);
         shader.SetInt("nTriangles", nTriangles);
+        shader.SetInt("nEdgeClusters", nEdgeClusters);
+        shader.SetInt("nTetClusters", nTetClusters);
 
         // Create buffers
         particleBuffer = new ComputeBuffer(nParticles, (3 * 3 + 1) * sizeof(float));
-        lcBuffer = new ComputeBuffer(nEdges, 2 * sizeof(uint) + sizeof(float));
-        vcBuffer = new ComputeBuffer(nTets, 4 * sizeof(uint) + sizeof(float));
+        lcBuffer = new ComputeBuffer(nEdges, 2 * sizeof(int) + sizeof(float));
+        vcBuffer = new ComputeBuffer(nTets, 4 * sizeof(int) + sizeof(float));
+        eicBuffer = new ComputeBuffer(nEdgeClusters, sizeof(int));
+        ecsBuffer = new ComputeBuffer(nEdges, sizeof(int));
+        ticBuffer = new ComputeBuffer(nTetClusters, sizeof(int));
+        tcsBuffer = new ComputeBuffer(nTets, sizeof(int));
 
-        // Constraint buffers are constant and can already be set
+        // Set buffer data
+        particleBuffer.SetData(particles);
         lcBuffer.SetData(lc);
         vcBuffer.SetData(vc);
-        particleBuffer.SetData(particles);
+        eicBuffer.SetData(edgesInCluster);
+        ecsBuffer.SetData(edgeClusters);
+        ticBuffer.SetData(tetsInCluster);
+        tcsBuffer.SetData(tetClusters);
 
         // Bind buffers
         kiPreSolve = shader.FindKernel("preSolve");
@@ -84,10 +111,14 @@ public class SoftBody : MonoBehaviour
         kiSolveEdges = shader.FindKernel("solveEdges");
         shader.SetBuffer(kiSolveEdges, "ps", particleBuffer);
         shader.SetBuffer(kiSolveEdges, "lc", lcBuffer);
+        shader.SetBuffer(kiSolveEdges, "edgesInCluster", eicBuffer);
+        shader.SetBuffer(kiSolveEdges, "edgeClusters", ecsBuffer);
 
         kiSolveVolumes = shader.FindKernel("solveVolumes");
         shader.SetBuffer(kiSolveVolumes, "ps", particleBuffer);
         shader.SetBuffer(kiSolveVolumes, "vc", vcBuffer);
+        shader.SetBuffer(kiSolveVolumes, "tetsInCluster", ticBuffer);
+        shader.SetBuffer(kiSolveVolumes, "tetClusters", tcsBuffer);
 
         kiPostSolve = shader.FindKernel("postSolve");
         shader.SetBuffer(kiPostSolve, "ps", particleBuffer);
@@ -102,15 +133,36 @@ public class SoftBody : MonoBehaviour
 
         for (int i = 0; i < nSubSteps; i++)
         {
-            // Dispatch with one thread per particle/constraint
+            // Dispatch with one thread per particle/cluster
             shader.Dispatch(kiPreSolve, (nParticles + 127) / 128, 1, 1);
-            shader.Dispatch(kiSolveEdges, (nEdges + 127) / 128, 1, 1);
-            shader.Dispatch(kiSolveVolumes, (nTets + 127) / 128, 1, 1);
+
+            for (int j = 0; j < nEdgeClusters; j++)
+            {
+                shader.SetInt("currentEdgeCluster", j);
+                shader.Dispatch(kiSolveEdges, (edgesInCluster[j] + 64) / 64, 1, 1);
+            }
+            for (int j = 0; j < nTetClusters; j++)
+            {
+                shader.SetInt("currentTetCluster", j);
+                shader.Dispatch(kiSolveVolumes, (tetsInCluster[j] + 64) / 64, 1, 1);
+            }
+
             shader.Dispatch(kiPostSolve, (nParticles + 127) / 128, 1, 1);
         }
 
         particleBuffer.GetData(particles);
         UpdateMeshVertices();
+    }
+
+    void OnDestroy()
+    {
+        particleBuffer.Release();
+        lcBuffer.Release();
+        vcBuffer.Release();
+        eicBuffer.Release();
+        ecsBuffer.Release();
+        ticBuffer.Release();
+        tcsBuffer.Release();
     }
 
     void InitializeDataFromTetMesh()
@@ -158,6 +210,8 @@ public class SoftBody : MonoBehaviour
             Vector3 e2 = new Vector3(tm.vertices[3 * lc[i].p2], tm.vertices[3 * lc[i].p2 + 1], tm.vertices[3 * lc[i].p2 + 2]);
             lc[i].l0 = (e2 - e1).magnitude;
         }
+
+        CreateEdgeClusters();
     }
 
     void InitializeVolumeConstraints(in TetMesh tm)
@@ -195,6 +249,8 @@ public class SoftBody : MonoBehaviour
             particles[vc[i].p3].invm = pInvMass;
             particles[vc[i].p4].invm = pInvMass;
         }
+
+        CreateTetClusters();
     }
 
     void InitializeSurfaceMesh(in TetMesh tm)
@@ -223,5 +279,83 @@ public class SoftBody : MonoBehaviour
         }
         mesh.vertices = vertices;
         mesh.RecalculateNormals();
+    }
+
+    void CreateEdgeClusters()
+    {
+        var edgeGraph = new UndirectedGraph<int, UndirectedEdge<int>>();
+
+        // Add vertex for each constraint
+        for (int i = 0; i < nEdges; i++)
+        {
+            edgeGraph.AddVertex(i);
+        }
+
+        // Add edge between each constraint that shares particle
+        for (int i = 0; i < nEdges - 1; i++)
+        {
+            for (int j = i + 1; j < nEdges; j++)
+            {
+                int[] c1 = { lc[i].p1, lc[i].p2 };
+                int[] c2 = { lc[j].p1, lc[j].p2};
+                if (c1.Intersect(c2).Any())
+                {
+                    edgeGraph.AddEdge(new UndirectedEdge<int>(i, j));
+                }
+            }
+        }
+
+        // Compute vertex coloring
+        var edgeColoring = new VertexColoringAlgorithm<int, UndirectedEdge<int>>(edgeGraph);
+        edgeColoring.Compute();
+
+        // Set up the clusters
+        nEdgeClusters = edgeColoring.Colors.Values.Distinct().Count();
+        edgesInCluster = new int[nEdgeClusters];
+        foreach (int color in edgeColoring.Colors.Values)
+        {
+            edgesInCluster[color]++;
+        }
+        var sortedColoring = from entry in edgeColoring.Colors orderby entry.Value ascending select entry.Key;
+        edgeClusters = sortedColoring.ToArray();
+    }
+
+    void CreateTetClusters()
+    {
+        var tetGraph = new UndirectedGraph<int, UndirectedEdge<int>>();
+
+        // Add vertex for each constraint
+        for (int i = 0; i < nTets; i++)
+        {
+            tetGraph.AddVertex(i);
+        }
+
+        // Add edge between each constraint that shares particle
+        for (int i = 0; i < nTets - 1; i++)
+        {
+            for (int j = i + 1; j < nTets; j++)
+            {
+                int[] c1 = { vc[i].p1, vc[i].p2, vc[i].p3, vc[i].p4 };
+                int[] c2 = { vc[j].p1, vc[j].p2, vc[j].p3, vc[j].p4 };
+                if (c1.Intersect(c2).Any())
+                {
+                    tetGraph.AddEdge(new UndirectedEdge<int>(i, j));
+                }
+            }
+        }
+
+        // Compute vertex coloring
+        var tetColoring = new VertexColoringAlgorithm<int, UndirectedEdge<int>>(tetGraph);
+        tetColoring.Compute();
+
+        // Set up the clusters
+        nTetClusters = tetColoring.Colors.Values.Distinct().Count();
+        tetsInCluster = new int[nTetClusters];
+        foreach (int color in tetColoring.Colors.Values)
+        {
+            tetsInCluster[color]++;
+        }
+        var sortedColoring = from entry in tetColoring.Colors orderby entry.Value ascending select entry.Key;
+        tetClusters = sortedColoring.ToArray();
     }
 }
